53b58af1b05971bf84eff1a2816309b9582ab4e1
BTNGaming <35198559+BTNGaming@users.noreply.github.com>
2021-01-14 00:26:59 -0800

    Update leveler.py

--- levelers/leveler.py
+++ levelers/leveler.py
@@ -3015,7 +3015,7 @@ class Leveler(commands.Cog):
         name_u_fnt = ImageFont.truetype(self.font_unicode_file, 24)
         label_fnt = ImageFont.truetype(font_bold_file, 16)
         exp_fnt = ImageFont.truetype(font_bold_file, 9)
-        large_fnt = ImageFont.truetype(font_bold_file, 24)
+        large_fnt = ImageFont.truetype(font_heavy_file, 24)
         symbol_u_fnt = ImageFont.truetype(self.font_unicode_file, 15)
 
         async def _write_unicode(text, init_x, y, font, unicode_font, fill):
