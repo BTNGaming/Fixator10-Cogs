14f1b272e0149d7acb3fad2a23c1001514a0b4c2
BTNGaming <35198559+BTNGaming@users.noreply.github.com>
2021-01-15 19:38:33 -0800

    Update leveler.py

--- levelers/leveler.py
+++ levelers/leveler.py
@@ -3685,11 +3685,11 @@ class Leveler(commands.Cog):
         return 100 * level + 200
 
     async def _level_exp(self, level: int):
-        return level * 200 + 100 * level * (level - 1) // 2
+        return level * 200 + 100 * level * (level - 1) // 1.5
 
     async def _find_level(self, total_exp):
         # this is specific to the function above
-        return int((2 / 278) * (9 + math.sqrt(81 + 1112 * total_exp)))
+        return int((1 / 278) * (9 + math.sqrt(81 + 1112 * total_exp)))
 
     async def char_in_font(self, unicode_char, font):
         for cmap in font["cmap"].tables:
